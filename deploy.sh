2378be04f653f4a5155b2c82debfbe1b1fe0eb2b
Rhyse Simpson <skat1@live.com.au>
2018-09-04 19:57:29 +1000

    Fix derp in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-ver=$(sed -n "s|^version=||p" $INSTALLER/module.prop)
-name=$(sed -n "s|^name=||p" $INSTALLER/module.prop | sed -r "s| |-|g")
+ver=$(sed -n "s|^version=||p" module.prop)
+name=$(sed -n "s|^name=||p" module.prop | sed -r "s| |-|g")
 zip -r $name-$ver.zip . -x ".git/*" "LICENSE" "deploy.sh" ".gitignore" "*.zip"
