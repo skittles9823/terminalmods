cf09858d5d369dff1d529bdc4a2966350b23b875
Rhyse Simpson <skat1@live.com.au>
2018-09-07 13:36:17 +1000

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 ver=$(sed -n "s|^version=||p" module.prop)
-name=$(sed -n "s|^name=||p" module.prop | sed -r "s| |-|g")
-zip -r $name-$ver.zip . -x ".git/*" "LICENSE" "deploy.sh" ".gitignore" "*.zip"
+name=$(sed -n "s|^name=||p" module.prop | sed "s| |-|g")
+zip -r "$name"-"$ver".zip . -x ".git/*" "LICENSE" "deploy.sh" ".gitignore" "*.zip"
